e12308173581c748a06e7758d7ca4aa5578b50a4
Alexey Lemberskiy <a.lemberskiy@1prime.ru>
2018-01-10 18:35:22 +0300

    3 - наращивание функционала

--- src/main/java/org/ea/VersioningDemo.java
+++ src/main/java/org/ea/VersioningDemo.java
@@ -1,11 +1,16 @@
 package org.ea;
 
+import entities.User;
+
 /**
  * Versioning demo created by Daniel Persson
  */
 public class VersioningDemo {
     public static void main(String args[]) {
+        User user = new User();
+        user.setName("Alexey");
         System.out.println("Hello explorers.");
+        System.out.println("user: " + user);
         System.out.println();
         System.out.println("This program is running version "+
                 VersioningDemo.class.getPackage().getImplementationVersion());
